5ab3921ff40d75d15394e5dc245f7ba57fb4dd81
pluslatte <debrisfield0201@gmail.com>
2025-01-13 22:46:16 +0900

    tailwindcss, melt-ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { createScrollArea, melt } from '@melt-ui/svelte';
+
 	let newPost = $state('');
 	let posts = $state(['Hello, Misskey', 'Hello, SvelteKit']);
 
@@ -7,6 +9,13 @@
 			posts.push(newPost);
 		}
 	}
+
+	const {
+		elements: { root, content, viewport, corner, scrollbarY, thumbY }
+	} = createScrollArea({
+		type: 'always',
+		dir: 'ltr'
+	});
 </script>
 
 <div class="container">
@@ -15,7 +24,27 @@
 		<button type="submit">Note</button>
 	</form>
 
-	{#each posts as post}
-		<div class="post">{post}</div>
-	{/each}
+	<div
+		use:melt={$root}
+		class="relative h-72 w-56 overflow-hidden rounded-md border border-neutral-700 bg-neutral-800/90 text-white shadow-lg"
+	>
+		<div use:melt={$viewport} class="h-full w-full rounded-[inherit]">
+			<div use:melt={$content}>
+				<div class="p-7">
+					<h4 class="mb-4 font-semibold leading-none">Endless Flavors</h4>
+					{#each posts as post}
+						<div>{post}</div>
+						<div role="separator" class="my-2 h-px w-full bg-magnum-800/30" />
+					{/each}
+				</div>
+			</div>
+		</div>
+		<div
+			use:melt={$scrollbarY}
+			class="flex h-full w-2.5 touch-none select-none border-l border-l-transparent bg-neutral-300/10 p-px transition-colors"
+		>
+			<div use:melt={$thumbY} class="relative flex-1 rounded-full bg-neutral-300/50" />
+		</div>
+		<div use:melt={$corner} />
+	</div>
 </div>
