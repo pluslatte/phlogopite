52eb18a902cb81d8620aa7503e2b56446b481b10
pluslatte <debrisfield0201@gmail.com>
2025-01-13 21:06:13 +0900

    base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let newPost = $state('');
+	let posts = $state(['Hello, Misskey', 'Hello, SvelteKit']);
+
+	function addPost() {
+		if (newPost.trim()) {
+			posts.push(newPost);
+		}
+	}
+</script>
+
+<div class="container">
+	<form class="form" onsubmit={addPost}>
+		<input type="text" bind:value={newPost} placeholder="Type something..." />
+		<button type="submit">Note</button>
+	</form>
+
+	{#each posts as post}
+		<div class="post">{post}</div>
+	{/each}
+</div>
