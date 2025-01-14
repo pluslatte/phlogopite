628ea9d133cd4dceb6fe9cba839f6a797aa25a27
pluslatte <debrisfield0201@gmail.com>
2025-01-14 22:18:17 +0900

    note

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import Button from '@/components/ui/button/button.svelte';
 	import { ResizablePane } from '@/components/ui/resizable';
 	import ResizableHandle from '@/components/ui/resizable/resizable-handle.svelte';
 	import ResizablePaneGroup from '@/components/ui/resizable/resizable-pane-group.svelte';
 	import ScrollArea from '@/components/ui/scroll-area/scroll-area.svelte';
+	import Textarea from '@/components/ui/textarea/textarea.svelte';
 	import ToggleGroupItem from '@/components/ui/toggle-group/toggle-group-item.svelte';
 	import ToggleGroup from '@/components/ui/toggle-group/toggle-group.svelte';
 
@@ -19,17 +21,14 @@
 <div class="flex p-2 h-full w-full justify-center">
 	<ResizablePaneGroup direction="horizontal" class="h-full max-w-[80rem] rounded-lg border">
 		<ResizablePane defaultSize={25}>
-			<div class="flex h-full items-center justify-center p-6">
-				<div class="min-h-60 mb-2 flex flex-col">
-					<textarea
-						bind:value={newPost}
-						placeholder="Type something..."
-						class="h-40 border-4 border-neutral-600 bg-neutral-600 text-white rounded-t-md"
-					></textarea>
-					<button onclick={addPost} class="h-10 bg-neutral-700 text-white rounded-b-md">
-						Note
-					</button>
-				</div>
+			<div class="p-2">
+				<form onsubmit={addPost}>
+					<div class="grid gap-4">
+						<Textarea bind:value={newPost} placeholder="Type something..." class="h-40 border"
+						></Textarea>
+						<Button type="submit">Note</Button>
+					</div>
+				</form>
 			</div>
 		</ResizablePane>
 		<ResizableHandle withHandle />
