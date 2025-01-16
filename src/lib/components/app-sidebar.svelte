1d6a2cbc5d1e5bb6f26569051718b831eef0c428
pluslatte <debrisfield0201@gmail.com>
2025-01-17 00:09:48 +0900

    separate

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -4,27 +4,16 @@
 	import Sidebar from './ui/sidebar/sidebar.svelte';
 
 	import Button from '@/components/ui/button/button.svelte';
-	import Textarea from '@/components/ui/textarea/textarea.svelte';
 	import Separator from '@/components/ui/separator/separator.svelte';
-	import Avatar from '@/components/ui/avatar/avatar.svelte';
-	import AvatarImage from '@/components/ui/avatar/avatar-image.svelte';
-	import AvatarFallback from '@/components/ui/avatar/avatar-fallback.svelte';
 	import ScrollArea from '@/components/ui/scroll-area/scroll-area.svelte';
-	import Input from '@/components/ui/input/input.svelte';
 
-	import IconSun from 'lucide-svelte/icons/sun';
-	import IconMoon from 'lucide-svelte/icons/moon';
-	import IconGlobe from 'lucide-svelte/icons/globe';
-	import IconSend from 'lucide-svelte/icons/send';
-	import IconRocket from 'lucide-svelte/icons/rocket';
 	import IconBell from 'lucide-svelte/icons/bell';
 	import IconSettings from 'lucide-svelte/icons/settings';
 
-	import { toggleMode } from 'mode-watcher';
-	import { api as misskeyApi, Stream } from 'misskey-js';
+	import { api as misskeyApi } from 'misskey-js';
 	import type { IResponse } from 'misskey-js/entities.js';
 	import { onMount } from 'svelte';
-	import AvatarFallBackAnim from './avatar-fall-back-anim.svelte';
+	import CreateNote from './create-note.svelte';
 
 	let { data } = $props();
 	const cli = new misskeyApi.APIClient({
@@ -60,47 +49,7 @@
 
 <Sidebar>
 	<SidebarHeader>
-		<form onsubmit={addNote} class="m-2">
-			<div class="grid gap-4">
-				<div class="flex flex-row">
-					<Avatar class="rounded-lg">
-						<AvatarImage src={self?.avatarUrl} alt={'@' + self?.username} />
-						<AvatarFallBackAnim />
-					</Avatar>
-					<div class="flex-frow grid grid-flow-row ml-2 text-sm">
-						<span class="font-bold text-ellipsis overflow-hidden">{self?.name}</span>
-						<span class="text-muted-foreground text-ellipsis overflow-hidden"
-							>{'@' + self?.username + '@' + data.server}</span
-						>
-					</div>
-				</div>
-				<Textarea bind:value={newNote} placeholder="Type something..." class="h-40 border"
-				></Textarea>
-				<div class="flex flex-row">
-					<Button onclick={toggleMode} variant="outline" size="icon">
-						<IconSun
-							class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-						/>
-						<IconMoon
-							class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-						/>
-						<span class="sr-only">Toggle theme</span>
-					</Button>
-					<div class="flex-grow"></div>
-					<Button variant="outline" size="icon" class="rounded-r-none border-r-0 shrink-0">
-						<IconGlobe />
-					</Button>
-					<Separator orientation="vertical" />
-					<Button variant="outline" size="icon" class="rounded-l-none border-l-0 shrink-0">
-						<IconRocket />
-					</Button>
-					<Separator orientation="vertical" class="mx-4" />
-					<Button type="submit" size="icon" class="shrink-0">
-						<IconSend class="h-4 w-4" />
-					</Button>
-				</div>
-			</div>
-		</form>
+		<CreateNote {data} />
 		<Separator />
 	</SidebarHeader>
 	<SidebarContent>
---
+++ src/lib/components/create-note.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+	import Button from '@/components/ui/button/button.svelte';
+	import Textarea from '@/components/ui/textarea/textarea.svelte';
+	import Separator from '@/components/ui/separator/separator.svelte';
+	import Avatar from '@/components/ui/avatar/avatar.svelte';
+	import AvatarImage from '@/components/ui/avatar/avatar-image.svelte';
+
+	import { api as misskeyApi } from 'misskey-js';
+	import AvatarFallBackAnim from './avatar-fall-back-anim.svelte';
+	import type { IResponse } from 'misskey-js/entities.js';
+	import { onMount } from 'svelte';
+	import { toggleMode } from 'mode-watcher';
+
+	import IconSun from 'lucide-svelte/icons/sun';
+	import IconMoon from 'lucide-svelte/icons/moon';
+	import IconGlobe from 'lucide-svelte/icons/globe';
+	import IconSend from 'lucide-svelte/icons/send';
+	import IconRocket from 'lucide-svelte/icons/rocket';
+
+	let { data } = $props();
+
+	let newNote = $state('');
+	let self: IResponse | null = $state(null);
+
+	const cli = new misskeyApi.APIClient({
+		origin: 'https://' + data.server,
+		credential: data.token
+	});
+
+	async function addNote() {
+		const request = cli.request('notes/create', {
+			visibility: 'home',
+			text: newNote
+		});
+
+		const result = await request;
+		if (result) {
+			// TODO: show created note in timeline if visible.
+			newNote = '';
+		} else {
+			console.error('Note creation failed');
+		}
+	}
+
+	onMount(() => {
+		cli.request('i', {}).then((got) => {
+			self = got;
+		});
+	});
+</script>
+
+<form onsubmit={addNote} class="m-2">
+	<div class="grid gap-4">
+		<div class="flex flex-row">
+			<Avatar class="rounded-lg">
+				<AvatarImage src={self?.avatarUrl} alt={'@' + self?.username} />
+				<AvatarFallBackAnim />
+			</Avatar>
+			<div class="flex-frow grid grid-flow-row ml-2 text-sm">
+				<span class="font-bold text-ellipsis overflow-hidden">{self?.name}</span>
+				<span class="text-muted-foreground text-ellipsis overflow-hidden"
+					>{'@' + self?.username + '@' + data.server}</span
+				>
+			</div>
+		</div>
+		<Textarea bind:value={newNote} placeholder="Type something..." class="h-40 border"></Textarea>
+		<div class="flex flex-row">
+			<Button onclick={toggleMode} variant="outline" size="icon">
+				<IconSun
+					class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+				/>
+				<IconMoon
+					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+				/>
+				<span class="sr-only">Toggle theme</span>
+			</Button>
+			<div class="flex-grow"></div>
+			<Button variant="outline" size="icon" class="rounded-r-none border-r-0 shrink-0">
+				<IconGlobe />
+			</Button>
+			<Separator orientation="vertical" />
+			<Button variant="outline" size="icon" class="rounded-l-none border-l-0 shrink-0">
+				<IconRocket />
+			</Button>
+			<Separator orientation="vertical" class="mx-4" />
+			<Button type="submit" size="icon" class="shrink-0">
+				<IconSend class="h-4 w-4" />
+			</Button>
+		</div>
+	</div>
+</form>
