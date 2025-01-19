de83ae6c2d67be15623249475fb1669a0ba96496
pluslatte <debrisfield0201@gmail.com>
2025-01-19 21:41:43 +0900

    なんかいらない ScrollArea が入っていた

--- src/lib/components/misskey-user-page.svelte
+++ src/lib/components/misskey-user-page.svelte
@@ -64,47 +64,42 @@
 
 <div>
 	{#if user}
-		<ScrollArea>
-			<div class="relative flex flex-row justify-center bg-gradient-to-b from-transparent to-black">
-				<img src={user.bannerUrl} alt={'banner'} class="z-[-1] h-[18rem] w-[48rem] object-cover" />
-				<div class="absolute bottom-4 left-4 mr-4">
-					<div class="flex flex-row rounded-xl bg-card bg-opacity-60 p-2">
-						<Avatar class="h-28 w-28 rounded-xl border-4 border-foreground">
-							<AvatarImage src={user.avatarUrl} />
-							<AvatarFallBackAnim />
-						</Avatar>
-						<div class="ml-4 flex max-h-52 flex-col">
-							{#if user.name}
-								<div class="flex-shrink-0 overflow-hidden text-ellipsis whitespace-nowrap text-3xl">
+		<div class="relative flex flex-row justify-center bg-gradient-to-b from-transparent to-black">
+			<img src={user.bannerUrl} alt={'banner'} class="z-[-1] h-[18rem] w-[48rem] object-cover" />
+			<div class="absolute bottom-4 left-4 mr-4">
+				<div class="flex flex-row rounded-xl bg-card bg-opacity-60 p-2">
+					<Avatar class="h-28 w-28 rounded-xl border-4 border-foreground">
+						<AvatarImage src={user.avatarUrl} />
+						<AvatarFallBackAnim />
+					</Avatar>
+					<div class="ml-4 flex max-h-52 flex-col">
+						{#if user.name}
+							<div class="flex-shrink-0 overflow-hidden text-ellipsis whitespace-nowrap text-3xl">
+								<MfmText
+									mfmNodes={mfm.parse(user.name)}
+									assets={{ host: user.host, emojis: user.emojis }}
+								/>
+							</div>
+						{/if}
+						<div
+							class="flex-shrink-0 overflow-hidden text-ellipsis whitespace-nowrap text-muted-foreground"
+						>
+							{'@' + user.username + (user.host ? '@' + user.host : '')}
+						</div>
+						{#if user.description}
+							<ScrollArea type="auto" class="my-2 pr-2">
+								<div class="min-w-0 whitespace-pre-wrap text-wrap" style="word-break: break-word;">
 									<MfmText
-										mfmNodes={mfm.parse(user.name)}
+										mfmNodes={mfm.parse(user.description)}
 										assets={{ host: user.host, emojis: user.emojis }}
 									/>
 								</div>
-							{/if}
-							<div
-								class="flex-shrink-0 overflow-hidden text-ellipsis whitespace-nowrap text-muted-foreground"
-							>
-								{'@' + user.username + (user.host ? '@' + user.host : '')}
-							</div>
-							{#if user.description}
-								<ScrollArea type="auto" class="my-2 pr-2">
-									<div
-										class="min-w-0 whitespace-pre-wrap text-wrap"
-										style="word-break: break-word;"
-									>
-										<MfmText
-											mfmNodes={mfm.parse(user.description)}
-											assets={{ host: user.host, emojis: user.emojis }}
-										/>
-									</div>
-								</ScrollArea>
-							{/if}
-						</div>
+							</ScrollArea>
+						{/if}
 					</div>
 				</div>
 			</div>
-		</ScrollArea>
+		</div>
 		<ToggleGroup type="single" bind:value={noteListType}>
 			<ToggleGroupItem value="normal">
 				<span>notes</span>
