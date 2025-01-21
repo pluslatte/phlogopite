721202962ee8ffbf85fb717890f7dd01f98a6e8f
pluslatte <debrisfield0201@gmail.com>
2025-01-21 22:46:04 +0900

    とりあえずリノート

--- src/lib/components/misskey-note-actions.svelte
+++ src/lib/components/misskey-note-actions.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-	import Button from '@/components/ui/button/button.svelte';
+	import Button, { buttonVariants } from '@/components/ui/button/button.svelte';
 	import { api as misskeyApi } from 'misskey-js';
 	import type { Note } from 'misskey-js/entities.js';
 	import { getContext } from 'svelte';
+	import {
+		DropdownMenu,
+		DropdownMenuGroup,
+		DropdownMenuGroupHeading,
+		DropdownMenuTrigger,
+		DropdownMenuContent,
+		DropdownMenuItem
+	} from '@/components/ui/dropdown-menu';
 
 	import IconReply from 'lucide-svelte/icons/reply';
 	import IconRepeat2 from 'lucide-svelte/icons/repeat-2';
@@ -24,15 +32,28 @@
 				console.error('renote failed');
 			});
 	};
+
+	const quote = () => {};
 </script>
 
 <div class="flex flex-row gap-8 p-2">
 	<Button variant="ghost" size="icon" class="rounded-full">
 		<IconReply class="h-4 w-4" />
 	</Button>
-	<Button variant="ghost" size="icon" class="rounded-full" onclick={renote}>
-		<IconRepeat2 class="h-4 w-4" />
-	</Button>
+	<DropdownMenu>
+		<DropdownMenuTrigger
+			class={buttonVariants({ variant: 'ghost', size: 'icon', class: 'rounded-full' })}
+		>
+			<IconRepeat2 class="h-4 w-4" />
+		</DropdownMenuTrigger>
+		<DropdownMenuContent>
+			<DropdownMenuGroup>
+				<!-- <DropdownMenuGroupHeading>Renote</DropdownMenuGroupHeading> -->
+				<DropdownMenuItem onclick={renote}>Renote</DropdownMenuItem>
+				<DropdownMenuItem onclick={quote}>Quote</DropdownMenuItem>
+			</DropdownMenuGroup>
+		</DropdownMenuContent>
+	</DropdownMenu>
 	<Button variant="ghost" size="icon" class="rounded-full">
 		<IconSmilePlus class="h-4 w-4" />
 	</Button>
