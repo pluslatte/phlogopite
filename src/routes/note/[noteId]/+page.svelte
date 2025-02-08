33e4b393468fc5cac5803e8c1e3df6e0cae9aef7
pluslatte <debrisfield0201@gmail.com>
2025-02-08 15:26:13 +0900

    ノートの詳細ページでノートを表示する

--- src/routes/note/[noteId]/+page.svelte
+++ src/routes/note/[noteId]/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import { api as misskeyApi } from 'misskey-js';
 	import type { PhlogopiteCookies } from '@/phlogopite-cookies';
+	import MisskeyRenote from '@/components/misskey-renote.svelte';
+	import MisskeyNoteActions from '@/components/misskey-note-actions.svelte';
+	import { Separator } from '@/components/ui/separator';
+	import MisskeyQuote from '@/components/misskey-quote.svelte';
+	import MisskeyNote from '@/components/misskey-note.svelte';
+	import { setContext } from 'svelte';
 
 	let {
 		data
@@ -18,6 +24,10 @@
 	if (!cli) {
 		console.error('no misskeyApiClient found');
 	}
+
+	setContext('client', {
+		cli
+	});
 </script>
 
 {#await cli.request('notes/show', { noteId: data.noteId })}
@@ -25,7 +35,24 @@
 		{'Loading...'}
 	</span>
 {:then note}
-	<span>
-		{note.text}
-	</span>
+	<div class="p-2 pr-4">
+		{#if note.renote && !note.text}
+			<MisskeyRenote renotedBy={note.user} renote={note.renote} />
+			<MisskeyNoteActions note={note.renote} />
+		{:else if note.renote && note.text}
+			<MisskeyQuote
+				quotedBy={note.user}
+				{note}
+				quote={note.renote}
+				withReply={note.reply != null}
+				withReplyOfQuote={note.renote.reply != null}
+			/>
+			<MisskeyNoteActions {note} />
+		{:else}
+			<MisskeyNote {note} withReply={note.reply != null} />
+			<MisskeyNoteActions {note} />
+		{/if}
+	</div>
+{:catch error}
+	<span>{error}</span>
 {/await}
