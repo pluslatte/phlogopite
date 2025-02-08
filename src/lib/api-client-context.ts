97f8dcd71f3003058354258ba05dd5007334e727
pluslatte <debrisfield0201@gmail.com>
2025-02-08 23:21:05 +0900

    layout.svelte で clientContext を作る

--- src/lib/api-client-context.ts
+++ src/lib/api-client-context.ts
@@ -1,9 +1,10 @@
 import { api as misskeyApi } from "misskey-js";
 import { getContext, setContext } from "svelte";
+
 let clientKey = Symbol('client');
 
-export function setApiClientContext(client: misskeyApi.APIClient) {
-    setContext(clientKey, client);
+export function setApiClientContext(misskeyApiClient: misskeyApi.APIClient) {
+    setContext(clientKey, misskeyApiClient);
 }
 
 export function getApiClientContext(): misskeyApi.APIClient {
